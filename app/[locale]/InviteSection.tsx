887b08d2fb98eea37c7b8a982105f8689abd815c
Filip Kowal <filip.kowal@protonmail.com>
2023-12-12 14:48:09 +0100

    Create location form in InviteSection

--- app/[locale]/InviteSection.tsx
+++ app/[locale]/InviteSection.tsx
@@ -3,9 +3,11 @@ import TextInput from "@/components/TextInput";
 import Dialog from "@/components/Dialog";
 import { useState } from "react";
 import Button from "@/components/Button";
+import Checkbox from "@/components/Checkbox";
 
 export default function InviteSection() {
   const [isOpen, setIsOpen] = useState(false);
+  const [isInterviewOnline, setIsInterviewOnline] = useState(true);
 
   return (
     <>
@@ -17,17 +19,51 @@ export default function InviteSection() {
       >
         Invite to an interview
       </Button>
-      <Dialog isOpen={isOpen} setIsOpen={setIsOpen}>
-        <h1 className="text-3xl uppercase font-title">Interview Details</h1>
+      <Dialog
+        isOpen={isOpen}
+        setIsOpen={setIsOpen}
+        title="Interview Details"
+        footer={
+          <div className="w-full flex justify-end">
+            <Button
+              type="primary"
+              name="Invite"
+              onClick={() => setIsOpen(false)}
+            >
+              Next
+            </Button>
+          </div>
+        }
+      >
         <form className="flex flex-col gap-6" method="dialog">
-          <label>
-            <span className="text-xl">Location</span>
-            <TextInput
-              name="location"
-              label="Address of the interview meeting"
-              autofocus
-            ></TextInput>
-          </label>
+          <span className="text-xl">Location</span>
+          <div className="flex gap-6">
+            <Checkbox
+              name="interviewType"
+              value="online"
+              checked={isInterviewOnline}
+              required
+              onChange={() => setIsInterviewOnline(true)}
+            >
+              Online
+            </Checkbox>
+            <Checkbox
+              name="interviewType"
+              value="inPerson"
+              checked={!isInterviewOnline}
+              onChange={() => setIsInterviewOnline(false)}
+            >
+              In person
+            </Checkbox>
+          </div>
+          <TextInput
+            name="location"
+            label={
+              isInterviewOnline
+                ? "Link to the meeting online"
+                : "Address of the interview meeting"
+            }
+          ></TextInput>
         </form>
       </Dialog>
     </>
