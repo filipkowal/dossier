e7abd3659dca6ef4b85579ec9b495327b500c02f
Filip Kowal <filip.kowal@protonmail.com>
2024-01-17 14:09:15 +0100

    Resize pdf on window resize

--- app/[locale]/[id]/CvAndCertificates.tsx
+++ app/[locale]/[id]/CvAndCertificates.tsx
@@ -1,7 +1,9 @@
 "use client";
-import React, { useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import { Document, Page } from "react-pdf";
 import { pdfjs } from "react-pdf";
+import "react-pdf/dist/Page/TextLayer.css";
+import "react-pdf/dist/Page/AnnotationLayer.css";
 
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
   "pdfjs-dist/build/pdf.worker.min.js",
@@ -14,20 +16,38 @@ const CvAndCertificates = ({
   cvAndCertificates: string[];
 }) => {
   const [numPages, setNumPages] = useState<number>();
+  const [parentWidth, setParentWidth] = useState<number>();
 
   function onDocumentLoadSuccess({ numPages }: { numPages: number }): void {
     setNumPages(numPages);
   }
 
+  const parentRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    function handleResize() {
+      if (parentRef.current && parentRef.current.offsetWidth < 1000) {
+        setParentWidth(parentRef.current.offsetWidth);
+      }
+    }
+
+    window.addEventListener("resize", handleResize);
+    handleResize(); // Initial width
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
-    <div className="w-full" id="cvAndCertificates">
+    <div className="w-full" id="cvAndCertificates" ref={parentRef}>
       {cvAndCertificates.map((path, index) => (
         <Document file={path} key={index} onLoadSuccess={onDocumentLoadSuccess}>
           {Array.from(new Array(numPages), (el, index) => (
             <Page
               key={`page_${index + 1}`}
               pageNumber={index + 1}
-              className="w-full"
+              width={parentWidth && parentWidth < 1000 ? parentWidth : 1000}
             />
           ))}
         </Document>
