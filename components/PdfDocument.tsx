6db460954a41435d9a20f10418c4b2bfbc4947c4
Filip Kowal <filip.kowal@protonmail.com>
2024-03-14 20:23:07 +0100

    Use file contant(url)  directly in react-pdf

--- components/PdfDocument.tsx
+++ components/PdfDocument.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from "react";
+import React, { useState } from "react";
 import { Document, Page } from "react-pdf";
 
 export default function PdfDocument({
@@ -10,15 +10,13 @@ export default function PdfDocument({
 }) {
   const [numPages, setNumPages] = useState(0);
 
-  const fileData = useMemo(() => ({ data: atob(fileContent) }), [fileContent]);
-
   const onDocumentLoadSuccess = ({ numPages }: { numPages: number }) => {
     setNumPages(numPages);
     // set parentWidth here based on your logic
   };
 
   return (
-    <Document file={fileData} onLoadSuccess={onDocumentLoadSuccess}>
+    <Document file={fileContent} onLoadSuccess={onDocumentLoadSuccess}>
       {Array.from(new Array(numPages), (el, index) => (
         <Page
           key={`page_${index + 1}`}
