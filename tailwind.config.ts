e3cdfb8d8afe842aabfd0edf70b9a3c38cb248fc
filipkowal <filip.kowal@protonmail.com>
2024-03-19 14:24:31 +0100

    Do not zoom on large screens

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -46,13 +46,6 @@ const config: Config = {
     require("@tailwindcss/forms"),
     require("tailwind-scrollbar-hide"),
     require("tailwindcss-animate"),
-    function ({ addBase }: { addBase: Function }) {
-      addBase({
-        "@screen 3xl": {
-          html: { zoom: "125%" }, // adjust the zoom level as needed
-        },
-      });
-    },
   ],
 };
 export default config;
