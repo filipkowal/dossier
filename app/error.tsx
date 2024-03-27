9735b8e011e2f633dbf8c0d02f62ba5809d21877
filipkowal <filip.kowal@protonmail.com>
2024-03-27 12:36:55 +0100

    Add root error page

---
+++ app/error.tsx
@@ -0,0 +1,9 @@
+"use client";
+import ErrorPage from "@/components/ErrorPage";
+import { type Locale } from "@/i18n-config";
+import { useParams } from "next/navigation";
+
+export default function Error({ error }: { error: Error }) {
+  const params = useParams() as { locale: Locale };
+  return <ErrorPage error={error} locale={params.locale} />;
+}
