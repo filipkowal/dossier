36822219ba6c6dbc0fa68a7a3d4457a9d2ca82ba
filipkowal <filip.kowal@protonmail.com>
2024-05-20 10:54:02 +0200

    Add blurred sample dossier to expired dossier page

--- app/[locale]/expired/page.tsx
+++ app/[locale]/expired/page.tsx
@@ -1,5 +1,6 @@
 import { Locale } from "@/i18n-config";
 import { getDictionary } from "@/utils";
+import SampleDossier from "../login/SampleDossier";
 
 export default async function Home({
   params,
@@ -10,9 +11,16 @@ export default async function Home({
   const dict = dictionary["dossierExpired"];
 
   return (
-    <div className="w-full h-[97vh] text-center flex flex-col align-center justify-center items-center">
-      <h2 className="font-title text-3xl mb-4">{dict["title"]}</h2>
-      <p>{dict["message"]}</p>
+    <div>
+      <div className="fixed z-10 bg-digitalent-gray-light text-digitalent-blue text-left p-12 align-middle shadow-xl top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full md:w-auto">
+        <div className="text-2xl font-title font-medium uppercase pb-8 flex justify-between">
+          <span className="pr-10 max-w-[90%]">{dict.title}</span>
+        </div>{" "}
+        <p>{dict["heading"]}</p>
+        <p>{dict["message"]}</p>
+      </div>
+
+      <SampleDossier />
     </div>
   );
 }
