5cb2c88c2dd4f1c3668dd16c6d91e97cd8edc52a
filipkowal <filip.kowal@protonmail.com>
2024-07-23 11:53:07 +0200

    Update translations

--- components/NotFound.tsx
+++ components/NotFound.tsx
@@ -7,14 +7,7 @@ export default function NotFound({
 }) {
   return (
     <div className="w-full h-[97vh] text-center flex flex-col align-center justify-center items-center">
-      <h2 className="font-title text-3xl mb-4">{dict["noCandidateId"]}</h2>
-      <p>{dict["verify"]}</p>
-      <div className="flex gap-2">
-        <span>{dict["example"]}</span>
-        <pre>
-          dossier.digitalent.cloud/de/<b>123</b>
-        </pre>
-      </div>
+      <h2 className="font-title text-3xl mb-4">{dict}</h2>
     </div>
   );
 }
