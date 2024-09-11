3ef132d212287339b14f7aeca9221935ea135f91
filipkowal <filip.kowal@protonmail.com>
2024-09-11 15:08:25 +0200

    Update todo icon size in dossierstatus

--- app/[locale]/[id]/DossierStatus.tsx
+++ app/[locale]/[id]/DossierStatus.tsx
@@ -11,7 +11,11 @@ export default function DossierStatus({ candidate }: { candidate: Candidate }) {
 
   return (
     <div className={`p-8 mb-8 bg-digitalent-green flex gap-6 items-center`}>
-      <Image src={ToDoList} alt="To Do List Icon" className="hidden md:block" />
+      <Image
+        src={ToDoList}
+        alt="To Do List Icon"
+        className="hidden md:block max-h-16 max-w-16"
+      />
       <div className="font-title">{candidate.dossierMessage}</div>
     </div>
   );
