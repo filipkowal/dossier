f2796945ed363e3ae196b66a3e4173d71a6d2d1c
filipkowal <filip.kowal@protonmail.com>
2024-09-11 13:44:16 +0200

    Add icon and change bg of DossierStatus

--- app/[locale]/[id]/DossierStatus.tsx
+++ app/[locale]/[id]/DossierStatus.tsx
@@ -1,6 +1,8 @@
 "use client";
 import { Candidate } from "@/utils";
 import { useRefetch } from "./RefetchContext";
+import ToDoList from "@/public/toDoList.png";
+import Image from "next/image";
 
 export default function DossierStatus({ candidate }: { candidate: Candidate }) {
   const { isRefetching, startRefetch, endRefetch } = useRefetch();
@@ -8,8 +10,9 @@ export default function DossierStatus({ candidate }: { candidate: Candidate }) {
   if (!candidate.dossierMessage?.length || isRefetching) return;
 
   return (
-    <div className={`p-8 mb-8 border text-center font-title`}>
-      {candidate.dossierMessage}
+    <div className={`p-8 mb-8 bg-digitalent-green flex gap-6 items-center`}>
+      <Image src={ToDoList} alt="To Do List Icon" className="hidden md:block" />
+      <div className="font-title">{candidate.dossierMessage}</div>
     </div>
   );
 }
