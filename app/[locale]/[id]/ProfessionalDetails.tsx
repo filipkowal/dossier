ac859cf9ab491961f9f9309e1d8af9ae5e9c1446
filipkowal <filip.kowal@protonmail.com>
2024-09-23 12:16:01 +0200

    Hide absent professional details

--- app/[locale]/[id]/ProfessionalDetails.tsx
+++ app/[locale]/[id]/ProfessionalDetails.tsx
@@ -15,12 +15,13 @@ export default function ProfessionalDetails({
   candidate: Candidate;
   user: User;
 }) {
-  const Detail = ({ title, value }: { title: string; value: ReactNode }) => (
-    <div className="flex gap-2 flex-wrap sm:flex-nowrap sm:justify-between sm:grid grid-cols-2">
-      <p className="nowrap min-w-fit">{title}: </p>
-      <p className="font-bold sm:font-normal">{value}</p>
-    </div>
-  );
+  const Detail = ({ title, value }: { title: string; value: ReactNode }) =>
+    value ? (
+      <div className="flex gap-2 flex-wrap sm:flex-nowrap sm:justify-between sm:grid grid-cols-2">
+        <p className="nowrap min-w-fit">{title}: </p>
+        <p className="font-bold sm:font-normal">{value}</p>
+      </div>
+    ) : null;
 
   return (
     <div className="flex flex-col gap-2">
