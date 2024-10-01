25874edadae19279f12ddb64f793f6e37b4902e7
filipkowal <filip.kowal@protonmail.com>
2024-10-01 13:04:14 +0200

    Move Dossier Status to the top for more consistant responsiveness

--- app/[locale]/[id]/DossierStatus.tsx
+++ app/[locale]/[id]/DossierStatus.tsx
@@ -7,13 +7,17 @@ export default function DossierStatus({ candidate }: { candidate: Candidate }) {
   if (!candidate.dossierMessage?.length) return;
 
   return (
-    <div className={`p-8 mb-8 bg-digitalent-green flex gap-6 items-center`}>
-      <Image
-        src={ToDoList}
-        alt="To Do List Icon"
-        className="hidden md:block max-h-16 max-w-16"
-      />
-      <div className="font-title">{candidate.dossierMessage}</div>
+    <div className="pt-4 sm:pt-20 sm:px-32 bg-digitalent-blue">
+      <div
+        className={`p-8 bg-digitalent-green flex gap-6 items-center text-white`}
+      >
+        <Image
+          src={ToDoList}
+          alt="To Do List Icon"
+          className="hidden md:block max-h-16 max-w-16"
+        />
+        <div className="font-title">{candidate.dossierMessage}</div>
+      </div>
     </div>
   );
 }
--- app/[locale]/[id]/page.tsx
+++ app/[locale]/[id]/page.tsx
@@ -59,36 +59,60 @@ export default async function Home({
   }
 
   return (
-    <div className="w-full sm:pt-16 xl:grid xl:grid-cols-[minmax(450px,1fr),2fr] 2xl:grid-cols-[minmax(250px,1fr),2fr]">
-      <div className="flex flex-col bg-digitalent-blue text-white sm:items-start xl:items-end md:px-8 lg:px-16 xl:px-0">
-        <div className="flex flex-col justify-end items-center md:items-start xl:h-[34vh] 3xl:h-[30vh] pt-16 md:px-16 2xl:px-32 md:w-[27rem] 2xl:w-[35rem] w-full">
-          <ImageWithPlaceholder
-            alt="Candidate image"
-            src={candidate.candidateImage}
-            className="rounded-full sm:w-52 sm:h-52 h-40 w-40"
-            width={160}
-            height={160}
-            placeholder={candidate?.gender === "f" ? AvatarFemale : AvatarMale}
-          />
-        </div>
-        <div className="block xl:hidden mt-16 -mb-16 px-8 sm:px-16 w-full">
-          <DossierStatus candidate={candidate} />
-        </div>
-        <div className="flex flex-col mt-20 xl:mt-8 sm:px-16 2xl:px-32 px-8 mb-12 min-h-[70vh]">
-          {/* Mobile */}
-          <div className="flex flex-col md:hidden font-title text-2xl gap-12 mb-16">
-            <div>
-              <h1>
-                {candidate.firstName} {candidate.lastName}
-              </h1>
-              <h1 className="text-base mt-4 text-digitalent-green">
-                {" " + dict.candidate.candidatesFor}:
-              </h1>
-              <h1 className="text-xl">{candidate.vacancyTitle}</h1>
-            </div>
-          </div>
+    <>
+      <DossierStatus candidate={candidate} />
 
-          <div className="hidden md:flex xl:hidden flex-col md">
+      <div className="w-full xl:grid xl:grid-cols-[minmax(450px,1fr),2fr] 2xl:grid-cols-[minmax(250px,1fr),2fr]">
+        <div className="flex flex-col bg-digitalent-blue text-white sm:items-start xl:items-end md:px-8 lg:px-16 xl:px-0">
+          <div className="flex flex-col justify-end items-center md:items-start xl:h-[34vh] 3xl:h-[30vh] pt-16 md:px-16 md:w-[27rem] 2xl:w-[35rem] w-full">
+            <ImageWithPlaceholder
+              alt="Candidate image"
+              src={candidate.candidateImage}
+              className="rounded-full sm:w-52 sm:h-52 h-40 w-40"
+              width={160}
+              height={160}
+              placeholder={
+                candidate?.gender === "f" ? AvatarFemale : AvatarMale
+              }
+            />
+          </div>
+          <div className="flex flex-col mt-20 xl:mt-8 sm:px-16 px-8 mb-12 min-h-[70vh]">
+            {/* Mobile */}
+            <div className="flex flex-col md:hidden font-title text-2xl gap-12 mb-16">
+              <div>
+                <h1>
+                  {candidate.firstName} {candidate.lastName}
+                </h1>
+                <h1 className="text-base mt-4 text-digitalent-green">
+                  {" " + dict.candidate.candidatesFor}:
+                </h1>
+                <h1 className="text-xl">{candidate.vacancyTitle}</h1>
+              </div>
+            </div>
+
+            <div className="hidden md:flex xl:hidden flex-col md">
+              <div className="max-w-[48rem] font-title flex text-3xl gap-12 mb-16">
+                <div className="text-digitalent-green">
+                  <h1>{dict.candidate.candidate}:</h1>
+                  <h1>{dict.candidate.vacancy}:</h1>
+                </div>
+                <div>
+                  <h1>
+                    {candidate.firstName} {candidate.lastName}
+                  </h1>
+                  <h1>{candidate.vacancyTitle}</h1>
+                </div>
+              </div>
+            </div>
+            {/* Mobile End */}
+
+            <ContactDetails candidate={candidate} dict={dict} />
+          </div>
+        </div>
+
+        <div className="flex flex-col">
+          {/* Desktop */}
+          <div className="hidden xl:flex flex-col pt-16 sm:px-16 2xl:px-32 bg-digitalent-blue text-white justify-end min-h-[34vh] 3xl:h-[30vh]">
             <div className="max-w-[48rem] font-title flex text-3xl gap-12 mb-16">
               <div className="text-digitalent-green">
                 <h1>{dict.candidate.candidate}:</h1>
@@ -102,84 +126,61 @@ export default async function Home({
               </div>
             </div>
           </div>
-          {/* Mobile End */}
+          {/* Desktop End */}
 
-          <ContactDetails candidate={candidate} dict={dict} />
-        </div>
-      </div>
+          <div className="flex flex-col my-12 sm:my-16 sm:px-16 md:px-24 lg:px-32 px-8">
+            <div className="max-w-[48rem]">
+              <h2 className="text-xl font-title mb-4 sm:mb-8 ">
+                {dict.candidate.professionalDetails}
+              </h2>
 
-      <div className="flex flex-col">
-        {/* Desktop */}
-        <div className="hidden xl:flex flex-col pt-16 sm:px-16 2xl:px-32 bg-digitalent-blue text-white justify-end min-h-[34vh] 3xl:h-[30vh]">
-          <DossierStatus candidate={candidate} />
+              <ProfessionalDetails
+                candidate={candidate}
+                user={user}
+                dict={dict}
+              />
 
-          <div className="max-w-[48rem] font-title flex text-3xl gap-12 mb-16">
-            <div className="text-digitalent-green">
-              <h1>{dict.candidate.candidate}:</h1>
-              <h1>{dict.candidate.vacancy}:</h1>
-            </div>
-            <div>
-              <h1>
-                {candidate.firstName} {candidate.lastName}
-              </h1>
-              <h1>{candidate.vacancyTitle}</h1>
+              <LongCandidateInfo candidate={candidate} dict={dict.candidate} />
             </div>
           </div>
+          <CvAndCertificates cvAndCertificates={candidate.file} />
         </div>
-        {/* Desktop End */}
+        <div className="z-10 flex fixed md:bottom-6 bottom-0 justify-center sm:gap-3 md:gap-6 w-full">
+          <InviteSection
+            dict={{
+              ...dict.inviteModal,
+              ...dict.mainButtons,
+              ...dict.toastMessages,
+            }}
+            user={user}
+            params={params}
+            candidateGender={candidate?.gender}
+            searchParams={searchParams}
+          />
 
-        <div className="flex flex-col my-12 sm:my-16 sm:px-16 md:px-24 lg:px-32 px-8">
-          <div className="max-w-[48rem]">
-            <h2 className="text-xl font-title mb-4 sm:mb-8 ">
-              {dict.candidate.professionalDetails}
-            </h2>
+          <RejectSection
+            dict={{
+              ...dict.rejectModal,
+              ...dict.mainButtons,
+              ...dict.toastMessages,
+            }}
+            id={id}
+            locale={locale}
+            candidateGender={candidate?.gender}
+            isRejectButtonVisible={user.isRejectButtonVisible}
+          />
 
-            <ProfessionalDetails
-              candidate={candidate}
-              user={user}
-              dict={dict}
-            />
-
-            <LongCandidateInfo candidate={candidate} dict={dict.candidate} />
-          </div>
+          <ContactSection
+            dict={{
+              ...dict.mainButtons,
+              ...dict.contactModal,
+              ...dict.toastMessages,
+            }}
+            relationshipManager={relationshipManager}
+            id={id}
+          />
         </div>
-        <CvAndCertificates cvAndCertificates={candidate.file} />
       </div>
-      <div className="z-10 flex fixed md:bottom-6 bottom-0 justify-center sm:gap-3 md:gap-6 w-full">
-        <InviteSection
-          dict={{
-            ...dict.inviteModal,
-            ...dict.mainButtons,
-            ...dict.toastMessages,
-          }}
-          user={user}
-          params={params}
-          candidateGender={candidate?.gender}
-          searchParams={searchParams}
-        />
-
-        <RejectSection
-          dict={{
-            ...dict.rejectModal,
-            ...dict.mainButtons,
-            ...dict.toastMessages,
-          }}
-          id={id}
-          locale={locale}
-          candidateGender={candidate?.gender}
-          isRejectButtonVisible={user.isRejectButtonVisible}
-        />
-
-        <ContactSection
-          dict={{
-            ...dict.mainButtons,
-            ...dict.contactModal,
-            ...dict.toastMessages,
-          }}
-          relationshipManager={relationshipManager}
-          id={id}
-        />
-      </div>
-    </div>
+    </>
   );
 }
