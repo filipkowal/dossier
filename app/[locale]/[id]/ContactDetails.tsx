980922141188a9d784ab177d524e92b529a7e656
filipkowal <filip.kowal@protonmail.com>
2024-10-01 14:35:47 +0200

    Simplify ContactDetails styling

--- app/[locale]/[id]/ContactDetails.tsx
+++ app/[locale]/[id]/ContactDetails.tsx
@@ -13,74 +13,79 @@ export default function ContactDetails({
   dict: Dictionary;
 }) {
   return (
-    <div className="flex flex-col leading-7 lg:flex-row xl:flex-col lg:gap-16 xl:gap-8 sm:gap-8 justify-between xl:w-[19rem]">
-      <div className="md:w-1/2 xl:w-full">
-        <h2 className="text-xl font-title mb-4 sm:mb-8">
-          {dict.candidate.contactDetails}
-        </h2>
-        {candidate?.phoneNumber ? <p>{candidate.phoneNumber}</p> : ""}
-        {candidate.email ? (
-          <div className="flex gap-2">
-            <p className="overflow-hidden text-ellipsis">{candidate.email}</p>
-            <CopyButton value={candidate.email} dict={dict.toastMessages} />
-          </div>
-        ) : (
-          ""
-        )}
+    <>
+      <h2 className="text-xl font-title mb-4 sm:mb-8">
+        {dict.candidate.contactDetails}
+      </h2>
+      <div className="flex flex-col leading-7 gap-8 xl:w-[19rem]">
+        <div>
+          {candidate?.phoneNumber ? <p>{candidate.phoneNumber}</p> : ""}
+          {candidate.email ? (
+            <div className="flex gap-2">
+              <p className="overflow-hidden text-ellipsis">{candidate.email}</p>
+              <CopyButton value={candidate.email} dict={dict.toastMessages} />
+            </div>
+          ) : (
+            ""
+          )}
 
-        {candidate.linkedIn && (
-          <Link
-            href={candidate.linkedIn}
-            target="_blank"
-            className="block whitespace-nowrap overflow-hidden text-ellipsis max-w-[100%]"
-          >
-            <Image
-              alt="linked-in"
-              src={LinkedInIcon}
-              className="inline mb-[0.3rem]"
-              width={18}
-            />{" "}
-            {candidate.linkedIn?.replace(/\/$/, "").slice(27)}
-            <Image
-              alt="open-linkedin"
-              src={ShareIcon}
-              className="inline mb-[0.3rem] ml-2"
-              width={20}
-            />
-          </Link>
-        )}
-      </div>
-      <div className="md:w-1/2 xl:w-full">
-        {candidate?.birthDate ? (
-          <p className="whitespace-nowrap">
-            {candidate.birthDate + " "}
-            {candidate?.candidateAge ? "(" + candidate.candidateAge + ")" : ""}
-          </p>
-        ) : (
-          ""
-        )}
+          {candidate.linkedIn && (
+            <Link
+              href={candidate.linkedIn}
+              target="_blank"
+              className="block whitespace-nowrap overflow-hidden text-ellipsis max-w-[100%]"
+            >
+              <Image
+                alt="linked-in"
+                src={LinkedInIcon}
+                className="inline mb-[0.3rem]"
+                width={18}
+              />{" "}
+              {candidate.linkedIn?.replace(/\/$/, "").slice(27)}
+              <Image
+                alt="open-linkedin"
+                src={ShareIcon}
+                className="inline mb-[0.3rem] ml-2"
+                width={20}
+              />
+            </Link>
+          )}
+        </div>
 
-        {candidate?.address?.street ? (
-          <p className="overflow-hidden text-ellipsis">
-            {candidate.address?.street || ""}
-          </p>
-        ) : (
-          ""
-        )}
-        {candidate?.address?.zip || candidate?.address?.city ? (
-          <p className="overflow-hidden text-ellipsis">
-            {(candidate.address?.zip ? candidate.address?.zip + " " : "") +
-              (candidate?.address?.city || "")}
-          </p>
-        ) : (
-          ""
-        )}
-        {candidate?.address?.country ? (
-          <p>{candidate.address?.country || ""}</p>
-        ) : (
-          ""
-        )}
+        <div>
+          {candidate?.birthDate ? (
+            <p className="whitespace-nowrap">
+              {candidate.birthDate + " "}
+              {candidate?.candidateAge
+                ? "(" + candidate.candidateAge + ")"
+                : ""}
+            </p>
+          ) : (
+            ""
+          )}
+
+          {candidate?.address?.street ? (
+            <p className="overflow-hidden text-ellipsis">
+              {candidate.address?.street || ""}
+            </p>
+          ) : (
+            ""
+          )}
+          {candidate?.address?.zip || candidate?.address?.city ? (
+            <p className="overflow-hidden text-ellipsis">
+              {(candidate.address?.zip ? candidate.address?.zip + " " : "") +
+                (candidate?.address?.city || "")}
+            </p>
+          ) : (
+            ""
+          )}
+          {candidate?.address?.country ? (
+            <p>{candidate.address?.country || ""}</p>
+          ) : (
+            ""
+          )}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
